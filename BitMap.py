b01b0ca02c3f21abdb178c2bbbb326f4d5d119b9
tylerholmes <tyler.gholmes@protonmail.com>
2022-05-03 00:25:41 -0400

    it kinda works

--- BitMap.py
+++ BitMap.py
@@ -5,14 +5,69 @@ def calculate_dx_dy(x0, y0, x1, y1, size_x, size_y):
     dy = y1 - y0 / size_y
     return dx, dy
 
-def fill_rectangle_array(x0, y0, x1, y1, dx, dy, size_x, size_y, pixels):
-    
+def calculate_xy_prime(x, y, dx, dy, cx, cy, loop_count):
+    x_prime = (x * dx) * (x * dx) - (y * dy) * (y * dy) + cx
+    y_prime = 2*(x * dx) * (y * dy) + cy
 
-    return pixels
+    for i in range(0, loop_count):
+        x_prime = (x_prime * dx) * (x_prime * dx) - (y_prime * dy) * (y_prime * dy) + cx
+        y_prime = 2*(y_prime * dx) * (y * dy) + cy
+        i += 1
+
+    return x_prime, y_prime
+
+def prime_number_cases(x_prime, y_prime, loop_count):
+    red = green = blue = 0
+    if x_prime >= 0:
+        if x_prime == float("inf"):
+            red = 255
+        elif x_prime > loop_count:
+            red = 128
+        elif x_prime <= loop_count:
+            red = 64
+        elif x_prime < .005:
+            red = 0
+
+    if x_prime < 0:
+        if x_prime == float("-inf"):
+            blue = 255
+        elif x_prime < -1 * loop_count:
+            blue = 255
+        elif x_prime >= -1 * loop_count:
+            blue = 64
+        elif x_prime < -.005:
+            blue = 0
+
+    if y_prime >= 0:
+        if y_prime == float("inf"):
+            green = 255
+        elif y_prime > loop_count:
+            green = 200
+        elif y_prime <= loop_count:
+            green = 200
+        elif y_prime < .005:
+            green = 0
+
+    if y_prime < 0:
+        if y_prime == float("-inf"):
+            green = 128
+        elif y_prime < -1 * loop_count:
+            green = 90
+        elif y_prime >= -1 * loop_count:
+            green = 64
+        elif y_prime < -.005:
+            green = 0
+
+    return red, green, blue
+
+def alter_pixels(img, pixels, loop_count, dx, dy, cx, cy):
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            x_prime, y_prime = calculate_xy_prime(i, j, dx, dy, cx, cy, loop_count)
+            red, green, blue = prime_number_cases(x_prime, y_prime, loop_count)
+            pixels[i,j] = (red, green, blue)
 
 def main():
-    file_name = input("Enter the file name: ")  
-
     # Get valid x and y coordinates
     while True:
         x0 = float(input("Enter x0: "))
@@ -57,10 +112,11 @@ def main():
     # Get dx and dy
     dx, dy = calculate_dx_dy(x0, y0, x1, y1, size_x, size_y)
 
-    pixels = [[0 for x in range(size_x)] for y in range(size_y)]
-
-    image = Image.new("RGB", (size_x, size_y), "black")
-    image.save(file_name + ".bmp")
+    # Create image using the prime number cases and corresponding RGB values
+    img = Image.new("RGB", (size_x, size_y), (0,0,0))
+    pixels = img.load()
+    alter_pixels(img, pixels, loop_count, dx, dy, cx, cy)
+    img.show()
 
 if __name__ == "__main__":
     main()
